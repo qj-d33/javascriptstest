ada5082b0f79e534a6d54d71b427527203aeb2bf
qj-d33 <allyting3@gmail.com>
2023-06-01 15:46:01 +0800

    更新

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TraditionEnumerable
+{
+    public class javascriptstest
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
